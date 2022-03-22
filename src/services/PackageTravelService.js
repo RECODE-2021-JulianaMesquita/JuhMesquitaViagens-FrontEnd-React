5d28f9dbf219436991cd4fd103f76d43df46ac4d
Juliana Mesquita <julianamariasousamesquita@id.uff.br>
2022-03-22 01:09:43 -0300

    Atualizando PAcote

--- src/services/PackageTravelService.js
+++ src/services/PackageTravelService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const PACKAGE_API_BASE_URL = "https://juhmesquitaviagens-back-end.herokuapp.com/packageTravel";
+const PACKAGE_API_BASE_URL = "https://juhmesquitaviagens-back-end.herokuapp.com/packageTravel/";
 
 class packageTravelService {
 
@@ -9,19 +9,19 @@ class packageTravelService {
     }
 
     createPackage(packageTravel) {
-        return axios.post(PACKAGE_API_BASE_URL, packageTravel);
+        return axios.post(PACKAGE_API_BASE_URL + '/create', packageTravel);
     }
 
     getPackageById(packageTravelId) {
-        return axios.get(PACKAGE_API_BASE_URL + '/' + packageTravelId);
+        return axios.get(PACKAGE_API_BASE_URL + '/getById/' + packageTravelId);
     }
 
     updatePackage(packageTravel, packageTravelId) {
-        return axios.put(PACKAGE_API_BASE_URL + '/' + packageTravelId, packageTravel);
+        return axios.put(PACKAGE_API_BASE_URL + '/update/' + packageTravelId, packageTravel);
     }
 
     deletePackage(packageTravelId) {
-        return axios.delete(PACKAGE_API_BASE_URL + '/' + packageTravelId);
+        return axios.delete(PACKAGE_API_BASE_URL + '/delete/' + packageTravelId);
     }
 }
 export default new packageTravelService();
