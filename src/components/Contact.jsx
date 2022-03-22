3a51b35195883d89fbc090aa7deaa9315d6e118e
Juliana Mesquita <julianamariasousamesquita@id.uff.br>
2022-03-22 00:54:55 -0300

    Alterando contato

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from 'react';
 import {
   MDBContainer,
   MDBRow,
@@ -8,59 +8,63 @@ import {
   MDBInput,
 } from "mdbreact";
 
-const Contact = () => {
-  return (
-    <MDBContainer>
-      <MDBRow>
-        <MDBCol md="6">
-          <form>
-            <p className="h5 text-center mb-4">Entre em contato</p>
-            <div className="grey-text">
-              <MDBInput
-                label="Nome"
-                icon="user"
-                group
-                type="text"
-                validate
-                error="wrong"
-                success="right"
-              />
-              <MDBInput
-                label="E-mail"
-                icon="envelope"
-                group
-                type="email"
-                validate
-                error="wrong"
-                success="right"
-              />
-              <MDBInput
-                label="Assunto"
-                icon="tag"
-                group
-                type="text"
-                validate
-                error="wrong"
-                success="right"
-              />
-              <MDBInput
-                type="textarea"
-                rows="2"
-                label="Mensagem"
-                icon="pencil-alt"
-              />
-            </div>
-            <div className="text-center">
-              <MDBBtn outline color="secondary">
-                Enviar
-                <MDBIcon far icon="paper-plane" className="ml-1" />
-              </MDBBtn>
-            </div>
-          </form>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-  );
-};
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <MDBContainer>
+          <MDBRow>
+            <MDBCol md="6">
+              <form>
+                <p className="h5 text-center mb-4">Entre em contato</p>
+                <div className="grey-text">
+                  <MDBInput
+                    label="Nome"
+                    icon="user"
+                    group
+                    type="text"
+                    validate
+                    error="wrong"
+                    success="right"
+                  />
+                  <MDBInput
+                    label="E-mail"
+                    icon="envelope"
+                    group
+                    type="email"
+                    validate
+                    error="wrong"
+                    success="right"
+                  />
+                  <MDBInput
+                    label="Assunto"
+                    icon="tag"
+                    group
+                    type="text"
+                    validate
+                    error="wrong"
+                    success="right"
+                  />
+                  <MDBInput
+                    type="textarea"
+                    rows="2"
+                    label="Mensagem"
+                    icon="pencil-alt"
+                  />
+                </div>
+                <div className="text-center">
+                  <MDBBtn outline color="secondary">
+                    Enviar
+                    <MDBIcon far icon="paper-plane" className="ml-1" />
+                  </MDBBtn>
+                </div>
+              </form>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </div>
+    );
+  }
+}
 
 export default Contact;
