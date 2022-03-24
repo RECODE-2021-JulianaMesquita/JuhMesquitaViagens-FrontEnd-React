3cd8cded68874148f8d0920c00335f5312e21fbb
Juliana Mesquita <julianamariasousamesquita@id.uff.br>
2022-03-24 20:34:35 -0300

    Retirando / do caminho api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://juhmesquitaviagens-back-end.herokuapp.com/'
+    baseURL: 'https://juhmesquitaviagens-back-end.herokuapp.com'
 });
 
 export default api
